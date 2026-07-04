b980b860ee8c0c57cb4d42dfad0c3c6d10dd75ce
Sandhiya9039 <ravichandran4558@gmail.com>
2026-07-04 17:01:39 +0530

    Fix static files

--- skillswap/settings.py
+++ skillswap/settings.py
@@ -115,14 +115,9 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 
 # Media files
 
